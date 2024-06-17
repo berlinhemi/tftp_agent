782565dda86c0a2e9e689df7074c8d1ca993ae8e
berlinhemi <dennn93@mail.ru>
2024-06-17 15:54:20 -0400

    add tests RRQ WRQ

--- test/tfpclient_test.cpp
+++ test/tfpclient_test.cpp
@@ -23,10 +23,8 @@ public:
 
 };
 
-TEST(SimpleTest, check)
+TEST(TFTPCliTest, CheckReadRequest)
 {
-    // Создаем mock-объект, который заменит Database
-    //MockUdpSocket* mock_socket = new MockUdpSocket(); 
     MockUdpSocket mock_socket;
 
     const std::string server_addr = "1.1.1.1";
@@ -48,45 +46,44 @@ TEST(SimpleTest, check)
     buf.insert(buf.end(), mode.begin(), mode.end());
     buf.push_back(0);
 
-    
-    //EXPECT_CALL(mock_socket, WriteDatagram(&buf[0], 10, server_addr.c_str(), port));
-    //const auto packet_size = std::distance((char*)&buf[0], end);
-    //EXPECT_CALL(mock_socket, WriteDatagram(&buf[0], packetSize, server_addr.c_str(), port));
-
-    //cant compare pointers (buf and server addr)!!!
+  
+    //do not compare pointers (buf and server addr)!!!
+    //RRQ
     EXPECT_CALL(mock_socket, WriteDatagram(buf, server_addr, port));
-
-     std::cout << "RUN CODE:";
-    std::cout << (int)tftp_cli.Get(fname);
-     std::cout << "\n";
-
-    //mock_socket.WriteDatagram("ff",1,"ff",44); WORKS FINE
+    tftp_cli.Get(fname);
     
-    //delete mock_socket;
-    //mock_socket = nullptr;
-    // Макрос EXPECT_CALL позволяет описать, 
-    // что должно произойти с методом make_query за время теста:
-    //
-    // а) метод make_query должен быть вызван строго один раз (WillOnce)
-    // б) методу make_query в качестве первого аргумента передадут "query"
-    // в) что передадут в качестве второго аргумента не важно
-    // г) метод make_query установит второй аргумент в значение "result"
-    // д) метод make_query вернет true
-    
-    // Запомните: это только описание того, что еще должно произойти в тесте!
-    
-
-     
-    // Создаем экземпляр класса Order
-    //Order<MockDatabase> order(&database); 
-    
-    
-    // Проверяем метод check и все сделанные предположения относительно 
-    // метода make_query. При этом make_query внутри метода check будет
-    // вести себя так, как мы ему указали. А все предположения относительно
-    // количества вызовов и аргмументов будут строго проверены.
-    //order.check(100); // ... здесь начинаются реальные действия
 }
+
+TEST(TFTPCliTest, CheckWriteRequest)
+{
+    MockUdpSocket mock_socket;
+
+    const std::string server_addr = "1.1.1.1";
+    uint16_t port = 69;
+    //TODO FIXTURE ?
+    TFTPClient tftp_cli(&mock_socket, server_addr,  port);
+
+    std::vector<BYTE> buf;
+    buf.push_back(0);
+    buf.push_back(static_cast<char>(TFTPClient::OpCode::WRQ));
+   
+    // filename
+    const std::string fname = "data.txt";
+    buf.insert(buf.end(), fname.begin(), fname.end());
+    buf.push_back(0);
+    
+    // mode
+    std::string mode("octet");
+    buf.insert(buf.end(), mode.begin(), mode.end());
+    buf.push_back(0);
+
+  
+    //WRQ
+    EXPECT_CALL(mock_socket, WriteDatagram(buf, server_addr, port));
+    tftp_cli.Put(fname);
+    
+}
+
 
 int main(int argc, char** argv)
 {   
