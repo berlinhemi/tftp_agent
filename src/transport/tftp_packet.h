cf6d850e863953b95575066fbcf1df3f409651b7
berlinhemi <dennn93@mail.ru>
2024-08-05 15:58:55 -0400

    add DataPacket type

--- src/transport/tftp_packet.h
+++ src/transport/tftp_packet.h
@@ -9,6 +9,47 @@ enum class OpCode {
         RRQ = 1, WRQ, DATA, ACK, ERR, OACK
     };
 
+class DataPacket
+{
+public:
+    const static uint16_t kMaxSize = 516;
+    
+    DataPacket()
+    {}
+
+    DataPacket(uint16_t block_id, const std::vector<BYTE>& buffer ):
+        block_id(block_id)
+    {
+        if(buffer.size() > kMaxDataSize)
+        {
+            throw std::length_error("Too big data chunk.");
+        }
+        data.assign(buffer.begin(), buffer.end());
+    }
+
+    void InitFromBigEndianVector(std::vector<BYTE> buffer)
+    {
+        block_id = (buffer[2] << 8) | buffer[3] ;
+        data.assign(buffer.begin() + kHeaderSize, buffer.end());
+    }
+
+    std::vector<BYTE> ToBigEndianVector()
+    {
+        std::vector<BYTE> buffer;
+        buffer.push_back(0);
+        buffer.push_back(opcode);
+        buffer.push_back((uint16_t)(block_id >> 8));
+        buffer.push_back((uint16_t)(block_id & 0x00FF));
+        std::copy(data.begin(), data.end(), std::back_inserter(data));
+        return buffer;
+    }
+private:
+    static const uint16_t kMaxDataSize = 512;
+    static const uint16_t kHeaderSize = 4;
+    uint16_t opcode = (uint16_t)OpCode::DATA;
+    uint16_t block_id;
+    std::vector<BYTE> data;
+};
 
 class RequestPacket
 {
