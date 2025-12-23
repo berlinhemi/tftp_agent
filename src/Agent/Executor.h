af60bb229e30272e2f36f9034e4c5525fe2aee34
berlinhemi <dennn93@mail.ru>
2025-12-23 12:44:41 -0500

    add: enum for storing error codes

--- src/Agent/Executor.h
+++ src/Agent/Executor.h
@@ -8,10 +8,27 @@
 typedef unsigned char BYTE;
 // static const size_t MAX_DATA_SIZE = 10*1024*1024; // 10 MB
 
+enum class ExecStatus {
+    Success = 0,
+    CommandNotFound = 127,
+    PermissionDenied = 126,
+    
+    // Custom codes
+    ForkFailed = 200,
+    PipeFailed = 201,
+    WaitFailed = 202,
+    ExeclFailed = 203,
+    ChildSignaled = 204,
+    ChildStopped = 205,
+    
+    // General errors
+    UnknownError = 300
+};
+
 struct CommandResult {
     std::string output;
     std::string error;
-    int exitCode;
+    ExecStatus exitCode;
     //bool timedOut; //TODO
 };
 
